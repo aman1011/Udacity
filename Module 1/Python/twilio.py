e2411d5db15bfc336c1e3bd7fe56edf729c4d298
Aman <aman.gaur1011@gmail.com>
2017-10-04 21:42:36 +0530

    Removing sensitive information used in twilio API
    Detected via GitGuardian

--- Module 1/Python/twilio.py
+++ Module 1/Python/twilio.py
@@ -1,7 +1,7 @@
 from twilio.rest import TwilioRestClient
 
-account_sid = "AC35db667c505091a095030979d636b16d"
-auth_token = "790500f2fea9093c1b385a795ed51e88"
+account_sid = "AC35db667c5XXXXXXXXXXXXXXXXXX"
+auth_token = "790500XXXXXXXXXXXXXXXXXXXXXXXX"
 
 # creating a client
 client = TwilioRestClient(account_sid, auth_token)
@@ -9,7 +9,7 @@ client = TwilioRestClient(account_sid, auth_token)
 # creating message.
 message = client.sms.message.create(
 	body = "Testing Twilio, Bitch",
-	to = "+919962130574",
-	from_ = "+919962130574",
+	to = "+919XXXXXXXXX",
+	from_ = "+9199XXXXXXXXXXXX",
 );
 print (message.sid)

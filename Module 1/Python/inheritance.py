7800511770b7c146e80455c81cd76157998459e5
Aman <aman.gaur1011@gmail.com>
2017-10-09 16:55:45 +0530

    Adding Child class
    Remembering that when an init of a class is called via Class and not it's object, giving self is mandatory.
    class Parent():
        ...
        ...
        ...
    
    # Difference between the two.
    Parent.__init__(self, other_variables)
    
    # Here the init will be called implicitly.
    parent = Parent(other_variables)
    parent.

--- Module 1/Python/inheritance.py
+++ Module 1/Python/inheritance.py
@@ -6,6 +6,22 @@ class Parent():
         self.last_name = last_name
         self.eye_color = eye_color
 
+class Child(Parent):
+
+    """ This is a child class inheriting last name and eye color
+        from the parent and having an additional instance variable
+        called number of toys. """
+    def __init__(self, last_name, eye_color, no_of_toys):
+        print("Child constructor called")
+        Parent.__init__(self, last_name, eye_color)
+        self.no_of_toys = no_of_toys
+
+
 # instantiating the class.
-billy_joe = Parent("joe", "blue")
-print(billy_joe.last_name)
+#billy_joe = Parent("joe", "blue")
+#print(billy_joe.last_name)
+
+# instantiating the class child.
+silly_joe = Child("joe", "blue", 5)
+print(silly_joe.last_name)
+print(silly_joe.no_of_toys)
